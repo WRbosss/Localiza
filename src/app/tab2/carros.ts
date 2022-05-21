bf70a3a728c95fdc9ac4bd2f7b5423f0eeb3bcbe
lucas GG <poweredlucas@gmail.com>
2022-05-20 22:30:17 -0300

    pushDataIntoArray function created.

---
+++ src/app/tab2/carros.ts
@@ -0,0 +1,9 @@
+export const all_cars_file = {
+    "Chevrolet":[
+        {"id":1, "model": "Onix", "url":"https://www.chevrolet.com.br/content/dam/chevrolet/mercosur/brazil/portuguese/index/cars/cars-subcontent/04-images/novo-onix-branco-summit.png?imwidth=419%22%7D"},
+        {"id":2, "model": "Cruze", "url":"https://secure-developments.com/commonwealth/brasil/gm_forms/assets/front/images/jellys/61eb1d424183d.png%22%7D"}
+    ],
+    "Fiat":[
+        
+    ]
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { all_cars_file } from './carros'
 
 @Component({
   selector: 'app-tab2',
@@ -8,15 +9,19 @@ import { Router } from '@angular/router';
 })
 export class Tab2Page {
 
-  //carro: any = {marca: '', modelo: '', foto:'', unique: []}
+  car: any = { descricao: '', preco: '' }
+  model: any = { modelo: '', foto:'', quantidade: 1, carros: [] }
+  brand: any = { marca: '', modelos: []}
   
-
-  carro: any = { id: Date.now(), marca:'', modelo:'', foto:'', descricao:'', preco:''}
   carros: any = []
+
+  all_brands: any = []
 
   constructor(private router: Router) {}
 
   ngOnInit() {
+    for (let key in all_cars_file) { this.all_brands.push(key) }
+
     this.carros = JSON.parse(localStorage.getItem('carros'))
 
     if (this.carros == null){
@@ -25,35 +30,77 @@ export class Tab2Page {
   }
 
   create() {
+    this.model.foto = "https://bobbyhadz.com/images/blog/typescript-create-type-from-object-keys/banner.webp"
     if (this.checkForEmpty()) { 
-      //this.clear()
       return 
     }
 
-    this.carros.push(Object.assign({}, this.carro))
-    localStorage.setItem("carros", JSON.stringify(this.carros))
+    this.pushDataIntoArray()
   }
 
-  checkForEmpty() {
-    let continue_not_allowed = false
-    for(let key in this.carro) {
-      if (this.carro[key] == '') {
-        //this.warnUserOf(key)
-        continue_not_allowed = true
+  pushDataIntoArray(){
+    let brand_in = false; let model_in = false
+
+    for (let b in this.carros){
+      if(this.carros[b].marca == this.brand.marca){
+        
+        for (let m in this.carros[b].modelos){
+          if(this.carros[b]['modelos'][m].modelo == this.model.modelo) {
+            this.carros[b]['modelos'][m]['carros'].push(Object.assign({}, this.car))
+            this.carros[b]['modelos'][m]['quantidade'] += 1
+
+            model_in = true
+            break
+          }
+        }
+
+        if (!model_in){
+          this.model['carros'].push(Object.assign({}, this.car))
+          this.carros[b]['modelos'].push(Object.assign({}, this.model))
+        }
+
+        brand_in = true
+        break
       }
+
     }
+
+    if(!brand_in){
+      console.log('brand not in')
+      this.model['carros'].push(Object.assign({}, this.car))
+      this.brand['modelos'].push(Object.assign({}, this.model))
+      this.carros.push(Object.assign({}, this.brand))
+      }
+
+    }
+
+  checkForEmpty() {
+    let continue_not_allowed = false 
+    if(this.brand.marca == ''){
+      alert('Marca está vazio')
+      continue_not_allowed = true
+    }
+
+    if (this.model.modelo == ''){
+      alert('modelo está vazio')
+      continue_not_allowed = true
+    }
+
+    if (this.car.descricao == ''){
+      alert('Descrição está vazio')
+      continue_not_allowed = true
+    }
+
+    if (this.car.preco == ''){
+      alert('Preço está vazio')
+      continue_not_allowed = true 
+    }
+
     return continue_not_allowed
   }
 
-  //warnUserOf(empty_area:string) {}
 
-  clear(){
-    this.carro.marca = ''
-    this.carro.modelo = ''
-    this.carro.foto = ''
-    this.carro.descricao = ''
-    this.carro.preco = ''
-  }
+  //warnUserOf(empty_area:string) {}
 
   button_Voltar(){
     this.router.navigate(['tabs/tab1']);
