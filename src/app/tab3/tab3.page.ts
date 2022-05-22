3531f8ad481849e3ff69a94266cb6436f8c9fd8a
WRbosss <waldyr.programing@gmail.com>
2022-05-21 21:48:33 -0300

    layout tabs 3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -8,9 +9,37 @@ import { Router } from '@angular/router';
 })
 export class Tab3Page {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,public alertController: AlertController) {}
   
   button_Voltar(){
     this.router.navigate(['tabs/tab1']);
+  }
+  
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Onix de 100k',
+      subHeader: '',
+      message: 'Tem certeza que deseja alugar esse carro',
+      buttons: [
+        {
+            text: 'Não Aceito',
+            handler: () => {
+                console.log('Disagree clicked');
+            }
+        },
+        {
+           text: 'Aceito',
+           handler: () => {
+            this.router.navigate(['tabs/tab1']);
+           }
+       }
+   ]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 }
