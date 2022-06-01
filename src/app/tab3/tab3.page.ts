4e56519aa7ae7fa85f257bd4af72349f556140c1
lucas GG <poweredlucas@gmail.com>
2022-06-01 19:29:30 -0300

    dada

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -13,10 +13,12 @@ export class Tab3Page {
 
   a_car: any
 
+  initial_date_not_null: Boolean = false
   initial_date_value = this.return_currentDate()
   initial_date: String = ''
   initial_date_active = false
 
+  final_date_not_null: Boolean = false
   final_date_value = this.return_currentDate()
   final_date: String = ''
   final_date_active = false
@@ -26,28 +28,40 @@ export class Tab3Page {
     this.route.queryParams.subscribe(params => {
       if(this.router.getCurrentNavigation().extras.state){
         this.a_car = this.router.getCurrentNavigation().extras.state
-        console.log(this.a_car)
+        
       }
+      console.log(this.a_car)
     })
 
   }
 
-return_currentDate(){ return format(new Date(), 'yyyy-MM-dd') + "T" + format(new Date(), 'HH:mm:ss') + "Z" }
+return_currentDate( ){ return format(new Date(), 'yyyy-MM-dd') + "T" + format(new Date(), 'HH:mm:ss') + "Z" }
 
 initialdate_changed(date_value){
   this.initial_date_value = date_value;
   this.initial_date = format(parseISO(date_value), 'HH:mm, dd MMM')
   this.initial_date_active = false
 
+  this.initial_date_not_null = true
 }
 
 finaldate_changed(date_value){
   this.final_date_value = date_value;
   this.final_date = format(parseISO(date_value), 'HH:mm, dd MMM')
   this.final_date_active = false
+
+  this.final_date_not_null = true
 }
 
   button_Voltar() {
+
+    this.initial_date = ''
+    this.final_date = ''
+
+    this.a_car = null
+
+    this.initial_date_not_null = false
+    this.final_date_not_null = false
     this.router.navigate(['tabs/tab1']);
   }
 
